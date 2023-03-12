bc31b26f71751569bb383da5f879a7c0c53d3752
Adrian Lantos <lantosadrian@gmail.com>
2023-03-12 16:24:42 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+    
+    $('.gallery_img').click(function(){
+        var src = $(this).attr('src'); 
+        $('#fullscreen img').attr('src', src);
+        $('#fullscreen').css('display', 'flex');
+        $('.nav').fadeOut();
+        $('#fullscreen').fadeIn();
+    });
+    $('#fullscreen').click(function(){
+        $(this).fadeOut(); 
+        $('.nav').fadeIn();
+    });
+
+    $('.nav_btn').click(() => {
+        $('.nav_btn_icon').toggleClass('inactive');
+        $('.red').toggleClass('active');
+    })
+
+    $(window).scroll(() => {
+        const nav = document.querySelector('.nav');
+
+        if(window.scrollY > nav.offsetHeight + 150) {
+                    nav.classList.add('active')
+                } else {
+                    nav.classList.remove('active')
+                }
+    })
+});
